dc598ee11c982a69eafe4e2d11e2ebbe487e9da4
HaThiLinh2204 <linhh4599@gmail.com>
2023-02-08 09:41:37 +0000

    add profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
   get 'static_pages/home'
   
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   resources :lists
   root 'static_pages#home'
-  #root "lists#index"
+#   root "lists#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
